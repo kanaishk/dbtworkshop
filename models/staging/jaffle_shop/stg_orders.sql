1eb717f3716246af802bd1bf88b3b7af2bb97996
Kanaishk Garg <kanaishkgarg@gmail.com>
2024-07-18 06:36:54 +0000

    Added seed example

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -3,7 +3,7 @@ with orders as (
         id as order_id, 
         user_id as customer_id, 
         order_date, 
-        status 
+        status
     from {{ source('jaffle_shop', 'orders') }}
 )
 select * 
